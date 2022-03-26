f5f7943f1f20bca25c02996db7b9a97c9e11d126
joeroyzman <90789879+joeroyzman@users.noreply.github.com>
2022-03-25 20:18:09 -0400

    dwrf

--- volleyball-blog/components/TopBorder.vue
+++ volleyball-blog/components/TopBorder.vue
@@ -6,8 +6,8 @@
 
     <nav class="navigation">
       <ul class="navigation__list navigation__list--inline">
-        <li class="titleHome"><a href="index" class="navigation__link navigation__link--is-active">Home</a></li>
-        <li class="titlePlayers"><a href="players" class="navigation__link">Meet Our Players</a></li>
+        <li class="titleHome"><a href="/pages/index.vue" class="navigation__link navigation__link--is-active">Home</a></li>
+        <li class="titlePlayers"><a href="/pages/players.vue" class="navigation__link">Meet Our Players</a></li>
       </ul>
     </nav>
 
