4e628f943498d205a5c90cbfc0b7599f4fe9641f
joeroyzman <90789879+joeroyzman@users.noreply.github.com>
2022-03-31 12:57:34 -0400

    dfsdfg

--- volleyball-blog/pages/players.vue
+++ volleyball-blog/pages/players.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div><TopBorder></TopBorder></div>
-        <div><Footer></Footer></div>
+        <div><TopBorder></TopBorder></div> 
+        <!-- <div><Footer></Footer></div> -->
         <h1>players</h1>
 
     </div>
@@ -13,6 +13,10 @@
 
 <style scoped>
 
-
+body
+* {
+ background-color:white;
+ background-image: url("https://imgur.com/8adMbw2.png") 
+} 
 
 </style>
