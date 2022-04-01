51a1401a49e759fe37b721c6d0adc915831de38e
enochhhhchan <90789900+enochhhhchan@users.noreply.github.com>
2022-04-01 12:57:26 -0400

    Revert "sasd"
    This reverts commit 7ded97e07acba1271799b1b034a3a0892fbde327.

--- volleyball-blog/components/Footer.vue
+++ volleyball-blog/components/Footer.vue
@@ -39,11 +39,8 @@
   padding: 0px 10px;
   margin-top: 3rem;
   font-size: 1.5rem;
-  color: aliceblue;
-  text-decoration: none;
+  left: 10rem;
   background-color: rgba(220, 203, 115, 1);
-  list-style-type: none;
-
 }
 
 
