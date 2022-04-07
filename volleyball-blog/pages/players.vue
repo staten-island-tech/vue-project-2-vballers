851ecd3a98d040b06d3d68084a71761914f3d19b
joeroyzman <90789879+joeroyzman@users.noreply.github.com>
2022-04-07 12:38:07 -0400

    sdg

--- volleyball-blog/pages/players.vue
+++ volleyball-blog/pages/players.vue
@@ -37,7 +37,7 @@ body
  background-image: url("https://i.imgur.com/p9Row1W.png");
  background-repeat: no-repeat;
  background-size: cover;
-  background-position: center center;
+  background-position: center;
   background-attachment: fixed;
 } 
 
