ea5526e427ec67711a4a44172550b8f1c6063105
joeroyzman <90789879+joeroyzman@users.noreply.github.com>
2022-05-05 12:42:41 -0400

    Revert "fghdfg"
    This reverts commit 33e493860b328283edba1a38010ff70609ff7e48.

--- volleyball-blog/components/PlayerCard.vue
+++ volleyball-blog/components/PlayerCard.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
-        <Nuxt-link to="/enoch" class="box">
+        <div class="box">
             <div class="container-text">Enoch Chan</div>
             <div class="headshotdiv"> <img class="headshot" src="https://www.pinclipart.com/picdir/middle/142-1421318_abdu-sentamu-person-image-placeholder-clipart.png " alt=""></div>
             <div class="player-position">Outside</div>
-        </Nuxt-link>
-        
+        </div>
 
     </div>
 </template>
