945999c9ec6d59051f3ed6557455efbc6c1a2c1b
enochhhhchan <90789900+enochhhhchan@users.noreply.github.com>
2022-05-16 13:11:39 -0400

    ssssssssss

--- volleyball-blog/components/PlayerPhoto.vue
+++ volleyball-blog/components/PlayerPhoto.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <div class="box">
+            <div class="pic"></div>
             <div class="box2"><img class="headshot" src="https://www.pinclipart.com/picdir/middle/142-1421318_abdu-sentamu-person-image-placeholder-clipart.png " alt=""></div>
+            <h2 class="bio">eee</h2>
         </div>
+
+        
+        
     </div>
 </template>
 
@@ -15,23 +20,48 @@
 .box{
     border: 10px solid white;
     width: 1250px;
-    height: 500px;
-    background-color: lightgray;
+    height: 950px;
+    background-color: white;
     margin-left: auto;
     margin-right: auto;
     margin-top: 10rem;
     margin-bottom: 200px;
+    bottom: 10px;
 }
+ .pic{
+     width: 1230px;
+     height: 480px;
+     background-color: gray;
+     left: 0px;
+ }
+
+ .bio{
+     background-color: transparent;
+     float: top;
+ }
 
 .box2{
     border: 10px solid rgba(220, 203, 115, 1);
     width: 250px;
     height: 250px;
     background-color: white;   
-    
-    top: 300px;
+    z-index: 0;
+    top: -150px;
     left: 30px;
 }
+
+.box3{
+    border: 10px solid white;
+    width: 1250px;
+    height: 500px;
+    background-color: lightgray;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5rem;
+    margin-bottom: 200px;
+    bottom: 70px;
+}
+
 
 .headshot {
     width: 90%;
